67b8258732f88b849e30b98f3c97828fbb28190e
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-06-19 18:30:23 -0700

    Updates

---
+++ precache-manifest.3b934d91a72cd642ae2e46fe43f03052.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a20a2f1bac300b3cfc8dfbaf649038",
+    "url": "/social-media-dashboard/index.html"
+  },
+  {
+    "revision": "6e01dca2d6d244501148",
+    "url": "/social-media-dashboard/static/js/2.0ad7e3b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-media-dashboard/static/js/2.0ad7e3b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a2f66c75816e2b7d4e",
+    "url": "/social-media-dashboard/static/js/main.4a68b7fa.chunk.js"
+  },
+  {
+    "revision": "0a933550a7469afd395a",
+    "url": "/social-media-dashboard/static/js/runtime-main.bb0a2bfe.js"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/social-media-dashboard/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/social-media-dashboard/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/social-media-dashboard/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/social-media-dashboard/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/social-media-dashboard/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/social-media-dashboard/static/media/icon-youtube.8499da12.svg"
+  }
+]);
