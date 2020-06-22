794eff789a4d0a5bb12d7a19c2bfc9433a1b76d8
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-06-22 10:59:11 -0700

    move global and theme to style directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from './theme';
-import { GlobalStyles } from './global';
+import { lightTheme, darkTheme } from './style/theme';
+import { GlobalStyles } from './style/global';
 import Overview from './components/Overview/Overview';
 import Dashboard from './components/Dashboard/Dashboard';
 import AppBar from './components/AppBar/AppBar';
